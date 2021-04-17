858efde52c9f7aa86b06a379e3b03f4ff5344c70
gzy0718 <18612863544@163.com>
2021-04-17 14:50:49 +0800

    新增了idea文件的忽略

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
 print("你好")
+try:
+    f= open("test3.py", "r")
+except:
+    f= open("text.py", "w")
+# print(num)
+try:
+    print(num)
+# except NameError:   两种方法，一种是已知错误类型，另一种是不知道错误类型Exception
+except Exception as msg:  #打印错误信息
+    print(msg)
