d57ded954c627b690c4e300e6356803c6cb97b46
gzy0718 <18612863544@163.com>
2021-04-24 01:14:06 +0800

    面向对象方法

--- 作业.py
+++ 作业.py
@@ -13,3 +13,28 @@
 # 2）每次跑步会减肥0.5公斤
 # 3）每次吃东西体重会增加1公斤
 # 4）小美的体重是45.0公斤
+class person():
+    def __init__(self,name,weigh):
+        self.name=name
+        self.weigh=weigh
+        print(f"{self.name}体重{self.weigh}公斤")
+    # def __str__(self):
+    #     return f"{self.name}爱跑步，{self.name}爱吃东西"
+
+    def run(self):
+        # print(f"{self.name}体重{self.weigh}公斤")
+        self.weigh-=0.5
+        print(f"每次跑步后的体重为{self.weigh}")
+
+    def eat(self):
+        self.weigh+=1
+        print(f"每次吃完东西的体重是{self.weigh}")
+xiaoming=person("小明",75)
+xiaoming.run()
+xiaoming.eat()
+xiaomei=person("小美",45.0)
+
+
+
+
+
